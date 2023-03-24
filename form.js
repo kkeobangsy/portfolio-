2212ae5876f6ae09dfb3fb4acbcfbadeec4f1416
K_dev11 <kkeobangsy@gmail.com>
2023-03-24 00:23:39 -0400

    Delete form.js

--- form.js
+++
@@ -1,5 +0,0 @@
-let form = naw Validator("contactform");
-
-form.addValidation("name", "req", "Please provide you name");
-form.addValidation("email", "req", "Please provide your email");
-form.addValidation("email", "email", "Please enter a valid email address");
